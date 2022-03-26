e3ee4b6edeacf7acf686a11b5f4c10b3a4b61765
Thiago <Thiagolopespereiralp1@gmail.com>
2022-03-25 21:24:39 -0300

    finalição do exercicio

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿double tempf, tempc;
+Console.Write("Digite a temperatura em graus Celsius: ");
+tempc = Convert.ToDouble(Console.ReadLine());
+
+tempf = tempc * 1.8 + 32;
+
+Console.WriteLine($"{tempc}°C equivalem a {tempf}°F");
